aee228fff92febe618ff35da4da552c165b3c347
Stephen Gladney <stephengladney@gmail.com>
2023-05-01 17:34:30 -0400

    rm runtime type evaluation in code and specs. not finished

--- src/convert.spec.ts
+++ src/convert.spec.ts
@@ -5,33 +5,81 @@ import {
   convertTime,
 } from "./convert"
 
-describe("STRING", () => {
+xdescribe("STRING", () => {
   const cases = [
-    "CamelCase",
-    "ConstCase",
-    "KabobCase",
-    "PascalCase",
-    "SnakeCase",
-    "String",
+    "camelCase",
+    "constCase",
+    "kabobCase",
+    "pascalCase",
+    "snakeCase",
+    "string",
   ]
 
-  const casedText = {
-    CamelCase: "helloWorld",
-    ConstCase: "HELLO_WORLD",
-    KabobCase: "hello-world",
-    PascalCase: "HelloWorld",
-    SnakeCase: "hello_world",
-    String: "hello world",
+  type CasedText = {
+    camelCase: string
+    constCase: string
+    kabobCase: string
+    pascalCase: string
+    snakeCase: string
+    string: string
+  }
+  const casedText: CasedText = {
+    camelCase: "helloWorld",
+    constCase: "HELLO_WORLD",
+    kabobCase: "hello-world",
+    pascalCase: "HelloWorld",
+    snakeCase: "hello_world",
+    string: "hello world",
   }
 
   cases.forEach((inputCase) => {
-    cases.forEach((outputCase) => {
-      test(`${inputCase} -> ${outputCase}`, () => {
+    if (inputCase !== "camelCase") {
+      test(`${inputCase} -> camelCase`, () => {
         expect(
-          convertString(casedText[inputCase])[`to${outputCase}`]()
-        ).toEqual(casedText[outputCase])
+          convertString(casedText[inputCase as keyof CasedText]).toCamelCase()
+        ).toEqual(casedText.camelCase)
       })
-    })
+    }
+
+    if (inputCase !== "constCase") {
+      test(`${inputCase} -> constCase`, () => {
+        expect(
+          convertString(casedText[inputCase as keyof CasedText]).toConstCase()
+        ).toEqual(casedText.constCase)
+      })
+    }
+
+    if (inputCase !== "kabobCase") {
+      test(`${inputCase} -> kabobCase`, () => {
+        expect(
+          convertString(casedText[inputCase as keyof CasedText]).toKabobCase()
+        ).toEqual(casedText.kabobCase)
+      })
+    }
+
+    if (inputCase !== "pascalCase") {
+      test(`${inputCase} -> pascalCase`, () => {
+        expect(
+          convertString(casedText[inputCase as keyof CasedText]).toPascalCase()
+        ).toEqual(casedText.pascalCase)
+      })
+    }
+
+    if (inputCase !== "snakeCase") {
+      test(`${inputCase} -> snakeCase`, () => {
+        expect(
+          convertString(casedText[inputCase as keyof CasedText]).toSnakeCase()
+        ).toEqual(casedText.snakeCase)
+      })
+    }
+
+    if (inputCase !== "string") {
+      test(`${inputCase} -> string`, () => {
+        expect(
+          convertString(casedText[inputCase as keyof CasedText]).toString()
+        ).toEqual(casedText.string)
+      })
+    }
   })
 })
 
@@ -39,15 +87,26 @@ describe("LENGTH", () => {
   const units = [
     "Millimeters",
     "Centimeters",
-    "Meters",
-    "Kilometers",
-    "Inches",
-    "Feet",
-    "Yards",
-    "Miles",
+    // "Meters",
+    // "Kilometers",
+    // "Inches",
+    // "Feet",
+    // "Yards",
+    // "Miles",
   ]
 
-  const twoMiles = {
+  type TwoMiles = {
+    Millimeters: number
+    Centimeters: number
+    Meters: number
+    Kilometers: number
+    Inches: number
+    Feet: number
+    Yards: number
+    Miles: number
+  }
+
+  const twoMiles: TwoMiles = {
     Millimeters: 3218687.99,
     Centimeters: 321868.8,
     Meters: 3218.688,
@@ -57,19 +116,109 @@ describe("LENGTH", () => {
     Yards: 3520,
     Miles: 2,
   }
+  type ResultMap = {
+    Millimeters: any
+    Centimeters: any
+    Meters: any
+    Kilometers: any
+    Inches: any
+    Feet: any
+    Yards: any
+    Miles: any
+  }
 
-  units.forEach((inputCase) => {
-    units.forEach((outputCase) => {
-      test(`${inputCase} -> ${outputCase}`, () => {
-        expect(
-          Number(
-            convertLength(twoMiles[inputCase])
-              [inputCase.toLowerCase()]()
-              [`to${outputCase}`]()
-              .toFixed(2)
-          )
-        ).toBeCloseTo(twoMiles[outputCase], 1)
-      })
+  const getResultMap = (inputCase: keyof ResultMap) => {
+    const resultMap: ResultMap = {
+      Millimeters: convertLength(
+        twoMiles[inputCase as keyof TwoMiles]
+      ).millimeters(),
+      Centimeters: convertLength(
+        twoMiles[inputCase as keyof TwoMiles]
+      ).centimeters(),
+      Meters: convertLength(twoMiles[inputCase as keyof TwoMiles]).meters(),
+      Kilometers: convertLength(
+        twoMiles[inputCase as keyof TwoMiles]
+      ).kilometers(),
+      Inches: convertLength(twoMiles[inputCase as keyof TwoMiles]).inches(),
+      Feet: convertLength(twoMiles[inputCase as keyof TwoMiles]).feet(),
+      Yards: convertLength(twoMiles[inputCase as keyof TwoMiles]).yards(),
+      Miles: convertLength(twoMiles[inputCase as keyof TwoMiles]).miles(),
+    }
+    return resultMap[inputCase]
+  }
+
+  units.forEach((inputCase, i) => {
+    test(`${inputCase.toLowerCase()} -> millimeters`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toMillimeters()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Millimeters, 1)
+    })
+    test(`${inputCase.toLowerCase()} -> centimeters`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toCentimeters()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Centimeters, 1)
+    })
+    test(`${inputCase.toLowerCase()} -> meters`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toMeters()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Meters, 1)
+    })
+    test(`${inputCase.toLowerCase()} -> kilometers`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toKilometers()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Kilometers, 1)
+    })
+    test(`${inputCase.toLowerCase()} -> inches`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toInches()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Inches, 1)
+    })
+    test(`${inputCase.toLowerCase()} -> feet`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toFeet()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Feet, 1)
+    })
+    test(`${inputCase.toLowerCase()} -> yards`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toYards()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Yards, 1)
+    })
+    test(`${inputCase.toLowerCase()} -> miles`, () => {
+      expect(
+        Number(
+          getResultMap(inputCase as keyof ResultMap)
+            .toMiles()
+            .toFixed(2)
+        )
+      ).toBeCloseTo(twoMiles.Miles, 1)
     })
   })
 
@@ -99,29 +248,29 @@ describe("TEMPERATURE", () => {
   })
 })
 
-describe("TIME", () => {
-  const units = ["Seconds", "Minutes", "Hours", "Days", "Weeks", "Years"]
-  const oneYear = {
-    Seconds: 31536000,
-    Minutes: 525600,
-    Hours: 8760,
-    Days: 365,
-    Weeks: 52.142857142857146,
-    Years: 1,
-  }
+// describe("TIME", () => {
+//   const units = ["Seconds", "Minutes", "Hours", "Days", "Weeks", "Years"]
+//   const oneYear = {
+//     Seconds: 31536000,
+//     Minutes: 525600,
+//     Hours: 8760,
+//     Days: 365,
+//     Weeks: 52.142857142857146,
+//     Years: 1,
+//   }
 
-  units.forEach((inputCase) => {
-    units.forEach((outputCase) => {
-      test(`${inputCase} -> ${outputCase}`, () => {
-        expect(
-          Number(
-            convertTime(oneYear[inputCase])
-              [inputCase.toLowerCase()]()
-              [`to${outputCase}`]()
-              .toFixed(2)
-          )
-        ).toBeCloseTo(oneYear[outputCase], 1)
-      })
-    })
-  })
-})
+//   units.forEach((inputCase) => {
+//     units.forEach((outputCase) => {
+//       test(`${inputCase} -> ${outputCase}`, () => {
+//         expect(
+//           Number(
+//             convertTime(oneYear[inputCase])
+//               [inputCase.toLowerCase()]()
+//               [`to${outputCase}`]()
+//               .toFixed(2)
+//           )
+//         ).toBeCloseTo(oneYear[outputCase], 1)
+//       })
+//     })
+//   })
+// })
