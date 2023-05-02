914d90648b65da185a2b2530bcf44866eca1fd6a
Stephen Gladney <stephengladney@gmail.com>
2023-05-01 21:28:45 -0400

    finish removing runtime eval from specs

--- src/convert.spec.ts
+++ src/convert.spec.ts
@@ -5,7 +5,7 @@ import {
   convertTime,
 } from "./convert"
 
-xdescribe("STRING", () => {
+describe("STRING", () => {
   const cases = [
     "camelCase",
     "constCase",
@@ -87,12 +87,12 @@ describe("LENGTH", () => {
   const units = [
     "Millimeters",
     "Centimeters",
-    // "Meters",
-    // "Kilometers",
-    // "Inches",
-    // "Feet",
-    // "Yards",
-    // "Miles",
+    "Meters",
+    "Kilometers",
+    "Inches",
+    "Feet",
+    "Yards",
+    "Miles",
   ]
 
   type TwoMiles = {
@@ -147,79 +147,96 @@ describe("LENGTH", () => {
     return resultMap[inputCase]
   }
 
-  units.forEach((inputCase, i) => {
-    test(`${inputCase.toLowerCase()} -> millimeters`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toMillimeters()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Millimeters, 1)
-    })
-    test(`${inputCase.toLowerCase()} -> centimeters`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toCentimeters()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Centimeters, 1)
-    })
-    test(`${inputCase.toLowerCase()} -> meters`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toMeters()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Meters, 1)
-    })
-    test(`${inputCase.toLowerCase()} -> kilometers`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toKilometers()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Kilometers, 1)
-    })
-    test(`${inputCase.toLowerCase()} -> inches`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toInches()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Inches, 1)
-    })
-    test(`${inputCase.toLowerCase()} -> feet`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toFeet()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Feet, 1)
-    })
-    test(`${inputCase.toLowerCase()} -> yards`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toYards()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Yards, 1)
-    })
-    test(`${inputCase.toLowerCase()} -> miles`, () => {
-      expect(
-        Number(
-          getResultMap(inputCase as keyof ResultMap)
-            .toMiles()
-            .toFixed(2)
-        )
-      ).toBeCloseTo(twoMiles.Miles, 1)
-    })
+  units.forEach((inputCase) => {
+    if (inputCase !== "Millimeters") {
+      test(`${inputCase.toLowerCase()} -> millimeters`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toMillimeters()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Millimeters, 1)
+      })
+    }
+
+    if (inputCase !== "Centimeters") {
+      test(`${inputCase.toLowerCase()} -> centimeters`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toCentimeters()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Centimeters, 1)
+      })
+    }
+    if (inputCase !== "Meters") {
+      test(`${inputCase.toLowerCase()} -> meters`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toMeters()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Meters, 1)
+      })
+    }
+    if (inputCase !== "Kilometers") {
+      test(`${inputCase.toLowerCase()} -> kilometers`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toKilometers()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Kilometers, 1)
+      })
+    }
+    if (inputCase !== "Inches") {
+      test(`${inputCase.toLowerCase()} -> inches`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toInches()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Inches, 1)
+      })
+    }
+    if (inputCase !== "Feet") {
+      test(`${inputCase.toLowerCase()} -> feet`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toFeet()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Feet, 1)
+      })
+    }
+    if (inputCase !== "Yards") {
+      test(`${inputCase.toLowerCase()} -> yards`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toYards()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Yards, 1)
+      })
+    }
+    if (inputCase !== "Miles") {
+      test(`${inputCase.toLowerCase()} -> miles`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toMiles()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(twoMiles.Miles, 1)
+      })
+    }
   })
 
   test("applies appropriate float", () => {
@@ -248,29 +265,114 @@ describe("TEMPERATURE", () => {
   })
 })
 
-// describe("TIME", () => {
-//   const units = ["Seconds", "Minutes", "Hours", "Days", "Weeks", "Years"]
-//   const oneYear = {
-//     Seconds: 31536000,
-//     Minutes: 525600,
-//     Hours: 8760,
-//     Days: 365,
-//     Weeks: 52.142857142857146,
-//     Years: 1,
-//   }
+describe("TIME", () => {
+  const units = ["Seconds", "Minutes", "Hours", "Days", "Weeks", "Years"]
 
-//   units.forEach((inputCase) => {
-//     units.forEach((outputCase) => {
-//       test(`${inputCase} -> ${outputCase}`, () => {
-//         expect(
-//           Number(
-//             convertTime(oneYear[inputCase])
-//               [inputCase.toLowerCase()]()
-//               [`to${outputCase}`]()
-//               .toFixed(2)
-//           )
-//         ).toBeCloseTo(oneYear[outputCase], 1)
-//       })
-//     })
-//   })
-// })
+  type OneYear = {
+    Seconds: number
+    Minutes: number
+    Hours: number
+    Days: number
+    Weeks: number
+    Years: number
+  }
+
+  const oneYear: OneYear = {
+    Seconds: 31536000,
+    Minutes: 525600,
+    Hours: 8760,
+    Days: 365,
+    Weeks: 52.142857142857146,
+    Years: 1,
+  }
+
+  type ResultMap = {
+    Seconds: any
+    Minutes: any
+    Hours: any
+    Days: any
+    Weeks: any
+    Years: any
+  }
+
+  const getResultMap = (inputCase: keyof ResultMap) => {
+    const resultMap: ResultMap = {
+      Seconds: convertTime(oneYear[inputCase as keyof OneYear]).seconds(),
+      Minutes: convertTime(oneYear[inputCase as keyof OneYear]).minutes(),
+      Hours: convertTime(oneYear[inputCase as keyof OneYear]).hours(),
+      Days: convertTime(oneYear[inputCase as keyof OneYear]).days(),
+      Weeks: convertTime(oneYear[inputCase as keyof OneYear]).weeks(),
+      Years: convertTime(oneYear[inputCase as keyof OneYear]).years(),
+    }
+    return resultMap[inputCase]
+  }
+
+  units.forEach((inputCase) => {
+    if (inputCase !== "Seconds") {
+      test(`${inputCase.toLowerCase()} -> seconds`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toSeconds()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(oneYear.Seconds, 1)
+      })
+    }
+    if (inputCase !== "Minutes") {
+      test(`${inputCase.toLowerCase()} -> minutes`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toMinutes()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(oneYear.Minutes, 1)
+      })
+    }
+    if (inputCase !== "Hours") {
+      test(`${inputCase.toLowerCase()} -> hours`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toHours()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(oneYear.Hours, 1)
+      })
+    }
+    if (inputCase !== "Days") {
+      test(`${inputCase.toLowerCase()} -> days`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toDays()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(oneYear.Days, 1)
+      })
+    }
+    if (inputCase !== "Weeks") {
+      test(`${inputCase.toLowerCase()} -> weeks`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toWeeks()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(oneYear.Weeks, 1)
+      })
+    }
+    if (inputCase !== "Years") {
+      test(`${inputCase.toLowerCase()} -> years`, () => {
+        expect(
+          Number(
+            getResultMap(inputCase as keyof ResultMap)
+              .toYears()
+              .toFixed(2)
+          )
+        ).toBeCloseTo(oneYear.Years, 1)
+      })
+    }
+  })
+})
